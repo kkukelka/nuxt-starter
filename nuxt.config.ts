414194f3d45e62f94e3a7dc173c6391c40627468
kkukelka <17953978+kkukelka@users.noreply.github.com>
2025-02-28 12:03:09 +0100

    chore: switch from postcss to vite plugin

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,14 +1,12 @@
 import { defineNuxtConfig } from 'nuxt/config'
+import tailwindcss from '@tailwindcss/vite'
 
 export default defineNuxtConfig({
   compatibilityDate: '2024-04-03',
   css: ['~/assets/css/main.css'],
   devtools: { enabled: true },
   modules: ['@nuxt/eslint'],
-  postcss: {
-    plugins: {
-      tailwindcss: {},
-      autoprefixer: {},
-    },
+  vite: {
+    plugins: [tailwindcss()],
   },
 })
